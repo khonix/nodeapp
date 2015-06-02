9bee39fa4df46012619b2e7c961a80c5a4cec036
khonix <khonix.neo@gmail.com>
2015-06-02 17:47:51 +0200

    Example with Express and Jade

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
 var express = require('express');
-app = express();
+var jade = require('jade');
 
+//Init Express
+var app = express();
+console.log('Express Initialized');
+
+//Set Views Folder
+app.set('views',__dirname + '/views');
+
+//Init Jade
+app.set('view engine','jade');
+console.log('Jade Initialized');
+
+//Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Render Index
 app.get('/',function(req,res){
-	res.render('index.ejs',{title: 'Hello World'});
-});
-
-app.get('/about',function(req,res){
-	res.render('layouts.ejs',{title: 'About Us', body: '<h1>About Us</h1>'});
-});
-
-app.get('/*',function(req,res){
-	res.status(404).render('error.ejs',{title: 'Error'});
+	res.render('index',{
+		title:'Welcome'
+	});
 });
 
 app.listen(3000);
+console.log('Sever running on localhost:3000');
